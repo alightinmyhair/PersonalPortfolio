e303c7aada4cb9371aa470340b485d9621e54edb
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-07-20 17:34:54 -0500

    Updated styling

--- script.js
+++ script.js
@@ -112,66 +112,6 @@ const TypeWriter = function(txtElement, words, wait = 2500) {
 
 }
 
-//Type Method
-//we need to use prototype 
-//to add a method to our TypeWriter
-// TypeWriter.prototype.type = function() {
-//     //current index of word
-//     const current = this.wordIndex % this.words.length;
-
-//     //Get full text of current word
-//     const fullTxt = this.words[current];
-
-//     //Check if deleting
-//     if(this.isDeleting) {
-//         //Remove char
-//         this.txt = fullTxt.substring(0, this.txt.length -1);
-//     }else {
-//         //Add char
-//         this.txt = fullTxt.substring(0, this.txt.length +1);
-//     }
-
-//     //Insert txt into element. the element is span
-//     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-//     //Initial Type Speed
-//     let typeSpeed = 200;
-
-//     if(this.isDeleting) {
-//         typeSpeed /=2;
-//     }
-
-//     //if word is complete
-//     if(!this.isDeleting && this.txt === fullTxt) {
-//         //make pause at end
-//         typeSpeed = this.wait;
-
-//         //set delete to true
-//         this.isDeleting = true;
-
-//     } else if(this.isDeleting && this.txt === '') {
-//         this.isDeleting = false;
-//         this.wordIndex++;
-//         //Pause before start type
-//         typeSpeed = 1000;
-
-//     }
-
-//     // timerId = setTimeout(() => this.type(), typeSpeed);
-//     this.timeoutId = setTimeout(() => this.type(), typeSpeed);
-//     // console.log('timeoutId: ', this.timeoutId);
-
-//     // clearTimeout(timerId);
-
-// }
-
-// TypeWriter.prototype.timeoutId;
-
-// TypeWriter.prototype.stopType = function() {
-//     console.log('timeout id:', this.timeoutId);
-//     clearTimeout(this.timeoutId);
-//     this.timeoutId = 0;
-// }
 
 //init on DOM Load
 document.addEventListener('DOMContentLoaded', startTypeWriter);
